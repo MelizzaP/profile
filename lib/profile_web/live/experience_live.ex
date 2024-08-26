3bbd202ec18bf7ed65cfb6ee0434bf5c9ae88efe
Melissa Patterson <melissa.patterson.va@gmail.com>
2024-08-26 12:19:41 -0400

    Mobile styling

--- lib/profile_web/live/experience_live.ex
+++ lib/profile_web/live/experience_live.ex
@@ -11,8 +11,8 @@ defmodule ProfileWeb.ExperienceLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <div :for={job <- @experience} class="p-4 mb-4 border-b-2 border-b-biege-light">
-      <div class="flex items-center justify-between">
+    <div :for={job <- @experience} class="md:p-4 p-2 mb-4 border-b-2 border-b-biege-light">
+      <div class="flex md:flex-row flex-col md:items-center justify-between">
         <h3 class="text-2xl font-bold underline"><%= job.company %></h3>
         <div>
           <%= Calendar.strftime(job.start_date, "%B, %Y") %> - <%= Calendar.strftime(
@@ -21,11 +21,11 @@ defmodule ProfileWeb.ExperienceLive do
           ) %>
         </div>
       </div>
-      <div class="flex items-center justify-between">
+      <div class="flex flex-col md:flex-row md:items-center justify-between">
         <h4 class="text-blue-light italic"><%= job.title %></h4>
         <div><%= job.location %> <span :if={job.remote}>- Remote</span></div>
       </div>
-      <ul class="flex justify-between w-full border-y border-biege-dark my-4 p-1 text-biege-dark">
+      <ul class="flex justify-start flex-wrap gap-2 md:justify-between w-full border-y border-biege-dark my-4 p-1 text-biege-dark">
         <li :for={tech <- job.tech_stack}><%= tech %></li>
       </ul>
       <ul class="flex flex-col gap-2 list-[square] leading-6 px-4 text-blue-light">
